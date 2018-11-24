a33a595b5b603923ddd0a532f8a5e0c58bbc27a1
zaidradd94 <zaidrddad@gmail.com>
2018-11-24 13:41:52 +0200

    port

--- server/index.js
+++ server/index.js
@@ -37,8 +37,9 @@ app.get('/items', function (req, res) {
     }
   });
 });
-
-app.listen(3000, function() {
-  console.log('listening on port 3000!');
+ 
+ var port = process.env.PORT || 3000
+app.listen(port, function() {
+  console.log(`listening on port ${port}`);
 });
 
